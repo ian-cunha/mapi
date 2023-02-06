374bac568b79f09b9f5782259f696fe20fe641fb
Ian <69704125+ian-cunha@users.noreply.github.com>
2023-02-06 07:53:11 -0300

    update

--- src/pages/Footer.tsx
+++ src/pages/Footer.tsx
@@ -9,7 +9,6 @@ function Footer() {
           <a className="wpp-footer" href=""><i className="bi bi-whatsapp"></i> Entre em contato</a>
           <a className="btn-instagram" href=""><i className="bi bi-instagram"></i></a>
           <a className="btn-facebook" href=""><i className="bi bi-facebook"></i></a>
-          <a className="btn-linkedin" href=""><i className="bi bi-linkedin"></i></a>
         </div>
       </div>
       <p className="text-center copy">© 2022 Mapi, Todos os direitos reservados.</p>
