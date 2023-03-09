1d714226f5d9fbbc9c28dea4f039d0196f9dd289
Ian <contatoiancunha@gmail.com>
2023-03-09 17:44:08 -0300

    patch 09/03

--- src/pages/Prestador.tsx
+++ src/pages/Prestador.tsx
@@ -3,7 +3,7 @@ function Prestador() {
     <div id="prestador" className="prestador">
       <div className="d-flex justify-content-evenly"><i className="bi bi-person-fill-gear fs-1"></i></div>
       <h2 className="text-center fs-1">É um Prestador de Serviços?</h2>
-      <h2 className="text-center fs-4">Venha fazer parte do Seu Mapa de Manutenções.</h2>
+      <h2 className="text-center fs-4">Venha fazer parte do Seu Mapa de Manutenções da sua região.</h2>
       <div className="d-flex justify-content-evenly"><a className="btn-pres text-center" target="_blank" href="https://api.whatsapp.com/send?phone=5581999170327&text=Ol%C3%A1,%20tenho%20interesse%20no%20Mapi!">Cadastre-se</a></div>
     </div>
   )
