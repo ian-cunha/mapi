36606a2b5ee5177c05c5d38e9668e08a224b61c5
Ian <69704125+ian-cunha@users.noreply.github.com>
2023-03-28 11:12:17 -0300

    v1.3

--- src/pages/WebApp.tsx
+++ src/pages/WebApp.tsx
@@ -3,21 +3,21 @@ import smartphone from "../assets/smartphone.svg";
 function WebApp() {
   return (
     <div className="web-app text-center d-flex flex-sm-row flex-column justify-content-evenly align-items-center">
-    
-    <div className="col-4 d-flex flex-column justify-content-between align-items-center">
-      <h3 className="btext">Informe de <b className="boldtext">forma efetiva</b> onde estão as suas demandas de manutenções.</h3>
-      <div className="d-flex flex-sm-row flex-column">
-      <button className="bbox1">Agilidade</button>
-      <button className="bbox2">Transparência</button>
-      <button className="bbox3">Inovação</button>
+      <div className="col-4 d-flex flex-column justify-content-between align-items-center">
+        <h2 className="btext">É gestor de imobiliária ou administrador de condomínio?</h2>
+        <h3 className="btext2">Informe de <b className="boldtext">forma efetiva</b> onde estão as suas demandas de manutenções.</h3>
+        <div className="d-flex flex-sm-row flex-column">
+          <p className="bbox1">Agilidade</p>
+          <p className="bbox2">Transparência</p>
+          <p className="bbox3">Inovação</p>
+        </div>
+        <button className="bbox4">Clique aqui e cadastre seus imóveis</button>
       </div>
-      <button className="bbox4">No MAPi, os melhores prestadores de serviço vão até você!</button>
-    </div>
 
-    <div className="col-4 imgorg">
-      <img className="smartphone" src={smartphone} alt="Smartphone" />
-    </div>
-    
+      <div className="col-4 imgorg">
+        <img className="smartphone" src={smartphone} alt="Smartphone" />
+      </div>
+
     </div>
   )
 }
